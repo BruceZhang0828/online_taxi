730bed11adfa0b6fb71dc3c5e2fc8b21d80bed5e
zhy <zycool0828@163.com>
2020-01-08 22:32:55 +0800

    网关机制

---
+++ taxi-gateway/src/main/java/com/zhy/taxi/gateway/OnlineTaxiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.zhy.taxi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @ClassName OnlineTaxiGatewayApplication
+ * @Description TODO 达叔写点注释吧 @Author 搬砖的孟达
+ * @Date 2020/1/8 22:15
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class OnlineTaxiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineTaxiGatewayApplication.class, args);
+    }
+}
